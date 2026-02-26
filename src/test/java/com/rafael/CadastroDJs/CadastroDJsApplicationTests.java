c922c54d4d72456e7377de2ded2096a74bce95de
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-26 14:19:33 -0300

    setup

---
+++ src/test/java/com/rafael/CadastroDJs/CadastroDJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.CadastroDJs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
